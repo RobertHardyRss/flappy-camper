2c8e81bc49a0356e1a2607b6b00be8817e79afd1
Robert Hardy <rhardy1@dcsdk12.org>
2022-09-22 17:20:43 -0600

    Add player movement.

--- scripts/app.js
+++ scripts/app.js
@@ -14,13 +14,45 @@ class Player {
 
 		this.x = 150;
 		this.y = CANVAS_HEIGHT / 2 + this.h / 2;
+
+		this.weight = 1;
+		this.vy = 0; //  the current velocity of y
+		this.vyMax = 5;
+		this.maxY = CANVAS_HEIGHT - 25 - this.h;
+
+		this.wireUpEvents();
 	}
 
-	update() {}
+	update() {
+		this.y += this.vy * this.weight;
+		this.y = Math.min(this.y, this.maxY);
+		this.vy += 0.1;
+
+		if (Math.abs(this.vy) > this.vyMax) {
+			if (this.vy > 0) {
+				this.vy = this.vyMax;
+			} else {
+				this.vy = this.vyMax * -1;
+			}
+		}
+	}
 
 	draw() {
 		this.ctx.fillStyle = "red";
 		this.ctx.fillRect(this.x, this.y, this.w, this.h);
+	}
+
+	flap() {
+		this.vy -= 2;
+	}
+
+	wireUpEvents() {
+		window.addEventListener("keypress", (ev) => {
+			console.log(ev);
+			if (ev.code === "Space") {
+				this.flap();
+			}
+		});
 	}
 }
 
